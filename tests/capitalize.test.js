c8485bb522c4ddc7a8a5b4cdf5043eab6aa2fb33
mukhtarkabir <mukhtarkabir3394@gmail.com>
2026-04-02 01:53:28 +0100

    Arrange files in respective folders.

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from "./capitalize.js";
+import { capitalize } from "../utilities/capitalize.js";
 
 test("takes a string and returns it with the first character capitalized", () => {
     expect(capitalize('fish')).toBe('Fish');
---
+++ tests/reverse-string.test.js
@@ -0,0 +1,3 @@
+test("takes a string and returns it reversed", () => {
+    expect(reverseString('fish')).toBe('hsif');
+});

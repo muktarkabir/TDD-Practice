4129803bfe3ec6f74040fdc57c57cdbc9f4819cf
mukhtarkabir <mukhtarkabir3394@gmail.com>
2026-04-02 02:02:00 +0100

    Make string reversing method pass test.

--- tests/reverse-string.test.js
+++ tests/reverse-string.test.js
@@ -1,3 +1,5 @@
+import reverseString from "../utilities/reverse-string.js";
 test("takes a string and returns it reversed", () => {
-    expect(reverseString('fish')).toBe('hsif');
+  expect(reverseString("fish")).toBe("hsif");
+  expect(reverseString("money")).toBe("yenom");
 });

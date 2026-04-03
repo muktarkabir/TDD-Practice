3d016038e7c5583f68b34b0c73a7bbc9fc28eccd
mukhtarkabir <mukhtarkabir3394@gmail.com>
2026-04-03 21:16:41 +0100

    Use 'describe' and 'it' syntax.

--- src/tests/caeser-cipher.test.js
+++ src/tests/caeser-cipher.test.js
@@ -1,8 +1,16 @@
 import { caeserCipher } from "../utilities/caeser-cipher.js";
 
-test("This test should encrypt a string using caeser-cipher based on a provided shift factor", () => {
-    expect(caeserCipher('abc',4)).toBe('efg');
-    expect(caeserCipher('xyz',3)).toBe('abc');
-    expect(caeserCipher('XYZ',3)).toBe('ABC');
-    expect(caeserCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+describe("Encryption using caeser-cipher algorithm", () => {
+  it("shifts based on a provided shift factor e.g 4", () => {
+    expect(caeserCipher("abc", 4)).toBe("efg");
+  });
+  it("wraps back to beginning of the alphabet when required", () => {
+    expect(caeserCipher("xyz", 3)).toBe("abc");
+  });
+  it("Is Case Sensitve", () => {
+    expect(caeserCipher("XYZ", 3)).toBe("ABC");
+  });
+  it("Maintains white spaces and special characters", () => {
+    expect(caeserCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+  });
 });
